86287f08be753e28cf6b26c5bff8681e26559430
parinda7 <109064939+parinda7@users.noreply.github.com>
2023-09-01 17:49:29 +0700

    uploadmy

---
+++ input.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="result.php" method="get">
+            A=<input type="text" name="a"><br>
+            B=<input type="text" name="b"><br>
+            <input type="submit" valuse="calculate">
+        </form>
+    </body>
+</html>
---
+++ result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+    $A = $_GET["a"];
+    $B = $_GET["b"];
+?>
+<?php
+
+while($B > 0) {
+  echo "$A <br>";
+  $B--;
+}
+?>
+</body>
+</html>
